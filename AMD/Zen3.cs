3ec9b55de3b0d42e045002a0a64d4625be2052f0
clamchowder <lamchester@gmail.com>
2021-03-28 19:09:20 -0700

    More untested Zen 3
    save progress

--- AMD/Zen3.cs
+++ AMD/Zen3.cs
@@ -134,5 +134,63 @@ namespace PmcReader.AMD
                         FormatLargeNumber(counterData.ctr3)};    
             }
         }
+
+        public class FetchConfig : MonitoringConfig
+        {
+            private Zen3 cpu;
+            public string GetConfigName() { return "Instruction Fetch"; }
+
+            public FetchConfig(Zen3 amdCpu) { cpu = amdCpu; }
+
+            public string[] GetColumns() { return columns; }
+
+            public void Initialize()
+            {
+                cpu.ProgramPerfCounters(GetPerfCtlValue(0x8E, 0x1F, true, true, false, false, true, false, 0, 0x1, false, false), // IC access
+                    GetPerfCtlValue(0x8E, 0x18, true, true, false, false, true, false, 0, 0x1, false, false),  // IC Miss
+                    GetPerfCtlValue(0x8F, 0x7, true, true, false, false, true, false, 0, 0x1, false, false),  // OC Access
+                    GetPerfCtlValue(0x8F, 0x4, true, true, false, false, true, false, 0, 0x1, false, false),  // OC Miss
+                    GetPerfCtlValue(0xAA, 0, true, true, false, false, true, false, 0, 0, false, false),  // uop from decoder
+                    GetPerfCtlValue(0xAA, 1, true, true, false, false, true, false, 0, 0, false, false)); // uop from op cache
+            }
+
+            public MonitoringUpdateResults Update()
+            {
+                MonitoringUpdateResults results = new MonitoringUpdateResults();
+                results.unitMetrics = new string[cpu.GetThreadCount()][];
+                cpu.InitializeCoreTotals();
+                for (int threadIdx = 0; threadIdx < cpu.GetThreadCount(); threadIdx++)
+                {
+                    cpu.UpdateThreadCoreCounterData(threadIdx);
+                    results.unitMetrics[threadIdx] = computeMetrics("Thread " + threadIdx, cpu.NormalizedThreadCounts[threadIdx]);
+                }
+
+                results.overallMetrics = computeMetrics("Overall", cpu.NormalizedTotalCounts);
+                results.overallCounterValues = cpu.GetOverallCounterValues("IC Access", "IC Miss", "OC Access", "OC Miss", "Decoder Ops", "OC Ops");
+                return results;
+            }
+
+            public string[] columns = new string[] { "Item", "Active Cycles", "Instructions", "IPC", "Op$ Hitrate", "Op$ MPKI", "Op$ Ops", "L1i Hitrate", "L1i MPKI", "Decoder Ops" };
+
+            public string GetHelpText()
+            {
+                return "";
+            }
+
+            private string[] computeMetrics(string label, NormalizedCoreCounterData counterData)
+            {
+                return new string[] { label,
+                        FormatLargeNumber(counterData.aperf),
+                        FormatLargeNumber(counterData.instr),
+                        string.Format("{0:F2}", counterData.instr / counterData.aperf),
+                        string.Format("{0:F2}%", 100 * (1 - counterData.ctr1 / counterData.ctr0)),
+                        string.Format("{0:F2}", 1000 * counterData.ctr1 / counterData.instr),
+                        FormatLargeNumber(counterData.ctr0 - counterData.ctr1),
+                        string.Format("{0:F2}%", 100 * (1 - counterData.ctr3 / counterData.ctr2)),
+                        string.Format("{0:F2}", 1000 * counterData.ctr3 / counterData.instr),
+                        FormatLargeNumber(counterData.ctr3 - counterData.ctr2),
+                        };
+            }
+        }
     }
 }
